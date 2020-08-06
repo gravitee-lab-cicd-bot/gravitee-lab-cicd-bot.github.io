24398919fba107eae3e5638b81f9c9e81672371c
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-08-06 23:40:30 +0200

    fix(./develop.sh): just some typos

--- oci/hugo_ide/build-n-run.sh
+++ oci/hugo_ide/build-n-run.sh
@@ -11,9 +11,13 @@ sed -i "s#baseurl =.*#baseURL = \"${HUGO_BASE_URL}\"#g" /gravitee-bot/src/config
 
 
 # Now building
+
 cd /gravitee-bot/src && rm -fr public
+echo "[- ./hugo/:/gravitee-bot/src/] volume folder content before hugo build : "
+ls -allh /gravitee-bot/src
 cd /gravitee-bot/src && hugo
 echo "[- ./hugo/:/gravitee-bot/src/] volume folder content after hugo build : "
 ls -allh /gravitee-bot/src
+read -p "DEBUG POINT" DEBUGPOINT
 echo "Gravitee Bot Doc HUGO SERVER boot"
 cd /gravitee-bot/src && hugo server
